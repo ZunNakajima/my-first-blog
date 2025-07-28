5b32decab4be5ef39c968226f4413bce9457a3ba
Jun-Nakajima <zunnakajima.it@gmail.com>
2025-07-28 23:32:45 +0900

    comitt

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # 開発時に静的ファイルを置く場所
+]
+
+#STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
